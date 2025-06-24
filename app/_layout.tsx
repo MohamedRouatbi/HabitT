73af53ca11805a44e15baaf4ff84c199a9e2f755
Mohamed <mohamed.rouatbi@eniso.u-sousse.tn>
2025-06-24 15:14:01 +0100

    h

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import React, { useEffect } from "react";
 
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuth = false;
+  useEffect(() => {
+    if (!isAuth) {
+      router.replace("/auth");
+    }
+  });
+  return <>{children}</>;
+}
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <RouteGuard>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </RouteGuard>
   );
 }
